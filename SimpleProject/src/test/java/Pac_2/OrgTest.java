fa7babaa8cfc4c5fdc529b89026dc44eca3146da
ShaikOsman24 <shaikosman2003@gmail.com>
2026-09-22 10:43:48 +0530

    Updated

--- SimpleProject/src/test/java/Pac_2/OrgTest.java
+++ SimpleProject/src/test/java/Pac_2/OrgTest.java
@@ -6,6 +6,15 @@ public class OrgTest {
 
 	@Test
 	public void CreateOrgTest() {
+		String url=System.getProperty("url");
+		String username=System.getProperty("username");
+		String pass=System.getProperty("pass");
+		String browser=System.getProperty("browser");
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(pass);
 		System.out.println("CreateOrgTest Created Successfully");
 	}
 	
